68921b697f88b82a3bd215db1e2a6ec29aacbf94
jaes <dlwo9503@naver.com>
2021-06-29 15:03:31 +0900

    JS 연습 #9

--- javascript-practices/ex12.js
+++ javascript-practices/ex12.js
@@ -1,11 +1,11 @@
-// Array 확장 : List 메서드 추가(prototype 확장)
+// Array 확장: List 메서드 추가(prototype 확장)
 
-Array.prototype.insert = function (index, value) {
-    if (value instanceof Array) {
-        // for (var i = 0; i < value.length; i++) {
-        //     this.splice(index++, 0, value[i]);
+Array.prototype.insert = function(index, value){
+    if(value instanceof Array){
+        // for(var i = 0; i < value.length; i++){
+        //    this.splice(index++, 0, value[i]);
         // }
-        var _this = this;
+        var _this = this;            
         value.forEach(function(e){
             _this.splice(index++, 0, e);
         });
@@ -14,7 +14,7 @@ Array.prototype.insert = function (index, value) {
     }
 }
 
-Array.prototype.remove = function (index) {
+Array.prototype.remove = function(index){
     this.splice(index, 1);
 }
 
@@ -28,15 +28,17 @@ console.log(a);
 a.remove(3);
 console.log(a);
 
-a.insert(2, ['a', 'b', 'c']);
-console.log(a);
+
+a.insert(2, ['a', 'b', 'c']); // 기대[1, 2, 'a', 'b', c', 3]
+console.log(a);               // 결과[1, 2, ['a', 'b', c'], 3]
+
 
 var f = function(param){
-    console.log(this + ":" + param);
+    console.log(this + ":" + param);    
 }
 
-// f();
+//f();
 
 o = { name:"me!" };
-f.call(o, "dlwo9503@naver.com"); // 바인딩 + 파라미터
-f.apply(o); // 바인딩
+f.call(o, "kickscar@gmail.com");
+f.apply(o);
