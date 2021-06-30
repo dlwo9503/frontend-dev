d73f990fbe7c80e5f6a2c8a91c0eddac0ee3a356
jaes <dlwo9503@naver.com>
2021-06-30 11:54:49 +0900

    ES6 #1

--- javascript-practices/ex6/ex01.js
+++ javascript-practices/ex6/ex01.js
@@ -1,7 +1,25 @@
-// 1. let: Block Scope의 변수 정의
+/**
+* let: Block Scope의 변수 정의
+*/
 
-if()
-var i = 10;
-let j = 20;
+// let의 블록 범위 
+try {
+    if(true) {
+        var i = 10;
+        let j = 20;
+    }
+    console.log(i, j);
+} catch(e) {
+    console.log("error:" + e);
+}
 
-// 2. const: Block Scope의 상수 정의
+// var의 함수 범위
+try {
+    var f = function() {
+        var m = 20;
+    }
+    f();
+    console.log(m);
+} catch(e) {
+    console.log("error:" + e);
+}
