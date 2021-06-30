f0a7b45a2d9ce98cdbfdf5b7c2607e5e8f9c537c
jaes <dlwo9503@naver.com>
2021-06-30 14:58:30 +0900

    es6 #3

---
+++ javascript-practices/ex6/ex03.js
@@ -0,0 +1,17 @@
+/*
+    3. Template Literal
+*/
+
+let no = 10;
+let name = "둘리";
+let email = "dlwo9503@naver.com";
+
+// ex1
+console.log("no:" + no + ", name:" + name + ", email:" + email);
+console.log(`no:${no}, name:${name}, email:${email}`);
+
+// ex2 - 다중행
+console.log(`no:${no}
+name:${name}
+email:${email}
+`);
